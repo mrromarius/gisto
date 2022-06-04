9ac9862304fd5b12dcc154a2ccfd8dfa59878bbb
Alexander Romashev <romashev.al.s@gmail.com>
2022-06-05 00:08:53 +0500

    Сделано сохранение в эксель. Убраны лишние формы

--- main.py
+++ main.py
@@ -1,21 +1,14 @@
-from ast import Assign
 import sys
-from PyQt5.QtWidgets import QApplication
+from PyQt5.QtWidgets import QApplication,  QMessageBox
 from PyQt5 import QtWidgets
 from PyQt5.QtCore import QRect, Qt
-from QueryTable1 import Ui_QueryTable1
-import pyqtgraph as pg
-import numpy as np
-
-
+from openpyxl import Workbook
 
 from MainWindow import Ui_MainWindow
 from Histogram import Ui_Histogram
 from Ontology import Ui_Ontology
-from QueryTable1 import Ui_QueryTable1
-from QueryTable2 import Ui_QueryTable2
-from QueryTable3 import Ui_QueryTable3
-from QueryTable4 import Ui_QueryTable4
+from QueryTable import Ui_QueryTable
+
 
 class GistoWindow(QtWidgets.QMainWindow, Ui_Histogram):
     def __init__(self):
@@ -31,7 +24,6 @@ class GistoWindow(QtWidgets.QMainWindow, Ui_Histogram):
     def click_load_gisto(self):
         # хороший способ реализации, но тогда нужно полностью сделать генерацию формы
         # https://www.cyberforum.ru/python-graphics/thread1513449.html#post7966919
-        print('load gistogram')
         
         # тут делаем жуткий хард-код потому что временя н еждет, можно потом переделать всю форму на динамическое создание
         ya_data = [int(self.lineEdit_1.text()),int(self.lineEdit_2.text()),int(self.lineEdit_3.text()),int(self.lineEdit_4.text()),int(self.lineEdit_5.text()), 
@@ -45,13 +37,17 @@ class GistoWindow(QtWidgets.QMainWindow, Ui_Histogram):
         int(self.lineEdit_41.text()),int(self.lineEdit_42.text()),int(self.lineEdit_43.text()),int(self.lineEdit_44.text()), int(self.lineEdit_45.text()),
         int(self.lineEdit_46.text()),int(self.lineEdit_47.text()),int(self.lineEdit_48.text()),int(self.lineEdit_49.text()), int(self.lineEdit_50.text())]
         y = [i for i in range(1, 52)]
+        # self.graphWidget.setBackground('w')
         self.graphWidget.clear()
+        
         self.graphWidget.setGeometry(QRect(410, 70, 800, 600))
-        self.graphWidget.plot(y, ya_data, stepMode=True, fillLevel = 0, brush=(0,0,255,150))
-        # забавный факт если передаем праметры в грид лайаут происходит ошибка, но зато вырвнивание работает нормально
-        # если убрать то все норм, но припервом запуске проиходит смещение графика
-        grid = QtWidgets.QGridLayout(self.centralwidget, 'fix_me')
-        grid.addWidget(self.graphWidget, 0, 0, Qt.AlignCenter)
+
+        self.graphWidget.plot(y, ya_data, stepMode=True, fillLevel = 0, brush=(140,150,0,255))
+        # self.graphWidget.plot([1,2,3,4,5],[2,3,4,3], stepMode=True, fillLevel = 0, brush=(140,150,0,255))
+        # self.graphWidget.plot([1,2,3,4,5],[5,3,4,9], stepMode=True, fillLevel = 0, brush=(140,230,0,2))
+
+        grid = QtWidgets.QGridLayout()
+        grid.addWidget(self.graphWidget, 3, 4, Qt.AlignCenter)
 
 class OntoWindow(QtWidgets.QMainWindow, Ui_Ontology):
     def __init__(self):
@@ -62,10 +58,11 @@ class OntoWindow(QtWidgets.QMainWindow, Ui_Ontology):
     def click_close(self):
         self.close()
 
-class Table1Window(QtWidgets.QMainWindow, Ui_QueryTable1):
-    def __init__(self):
+class TableWindow(QtWidgets.QMainWindow, Ui_QueryTable):
+    def __init__(self, tableName):
         super().__init__()
         self.setupUi(self)
+        self.tableName = tableName
         self.pushButton.clicked.connect(self.click_close)
         self.pushButton_3.clicked.connect(self.click_save_to_excel)
 
@@ -73,37 +70,103 @@ class Table1Window(QtWidgets.QMainWindow, Ui_QueryTable1):
         self.close()
 
     def click_save_to_excel(self):
-        # https://ru.stackoverflow.com/questions/1119966/%D0%9A%D0%B0%D0%BA-%D1%81%D0%BE%D1%85%D1%80%D0%B0%D0%BD%D0%B8%D1%82%D1%8C-%D0%B4%D0%B0%D0%BD%D0%BD%D1%8B%D0%B5-%D0%B8%D0%B7-qtablewidget-%D0%B2-xls
-        # https://ru.stackoverflow.com/questions/1270717/%D0%BA%D0%B0%D0%BA-%D1%81%D0%BE%D1%85%D1%80%D0%B0%D0%BD%D1%8F%D1%82%D1%8C-%D1%82%D0%B0%D0%B1%D0%BB%D0%B8%D1%86%D1%83-%D0%B2-%D0%BF%D1%80%D0%B8%D0%BB%D0%BE%D0%B6%D0%B5%D0%BD%D0%B8%D0%B8-%D0%B2-%D1%84%D0%B0%D0%B9%D0%BB-excel
-        # https://www.cyberforum.ru/python-graphics/thread2220443.html
-        print('смотри комменты')
+        excel_file = Workbook()
+        excel_sheet = excel_file.create_sheet(title = self.tableName, index = 0)
+        for count in range(1,30, 7):
+            excel_sheet.cell(row=count, column=1).value = "Запрос"
+            excel_sheet.cell(row=count+1, column=1).value = "Источник"
+            excel_sheet.cell(row=count+1, column=2).value = "Оценка"
+            excel_sheet.cell(row=count+1, column=3).value = "Тип ресурса"
 
-class Table2Window(QtWidgets.QMainWindow, Ui_QueryTable2):
-    def __init__(self):
-        super().__init__()
-        self.setupUi(self)
-        self.pushButton.clicked.connect(self.click_close)
+        excel_sheet['B1'] = self.textEditZapros1.toPlainText()
+        excel_sheet['B8'] = self.textEditZapros2.toPlainText()
+        excel_sheet['B15'] = self.textEditZapros3.toPlainText()
+        excel_sheet['B22'] = self.textEditZapros4.toPlainText()
+        excel_sheet['B29'] = self.textEditZapros5.toPlainText()
 
-    def click_close(self):
-        self.close()
+        excel_sheet.cell(row=3, column=1).value = self.textEdit_source_1.toPlainText()
+        excel_sheet.cell(row=4, column=1).value = self.textEdit_source_2.toPlainText()
+        excel_sheet.cell(row=5, column=1).value = self.textEdit_source_3.toPlainText()
+        excel_sheet.cell(row=6, column=1).value = self.textEdit_source_4.toPlainText()
+        excel_sheet.cell(row=7, column=1).value = self.textEdit_source_5.toPlainText()
+        excel_sheet.cell(row=10, column=1).value = self.textEdit_source_6.toPlainText()
+        excel_sheet.cell(row=11, column=1).value = self.textEdit_source_7.toPlainText()
+        excel_sheet.cell(row=12, column=1).value = self.textEdit_source_8.toPlainText()
+        excel_sheet.cell(row=13, column=1).value = self.textEdit_source_9.toPlainText()
+        excel_sheet.cell(row=14, column=1).value = self.textEdit_source_10.toPlainText()
+        excel_sheet.cell(row=17, column=1).value = self.textEdit_source_11.toPlainText()
+        excel_sheet.cell(row=18, column=1).value = self.textEdit_source_12.toPlainText()
+        excel_sheet.cell(row=19, column=1).value = self.textEdit_source_13.toPlainText()
+        excel_sheet.cell(row=20, column=1).value = self.textEdit_source_14.toPlainText()
+        excel_sheet.cell(row=21, column=1).value = self.textEdit_source_15.toPlainText()
+        excel_sheet.cell(row=24, column=1).value = self.textEdit_source_16.toPlainText()
+        excel_sheet.cell(row=25, column=1).value = self.textEdit_source_17.toPlainText()
+        excel_sheet.cell(row=26, column=1).value = self.textEdit_source_18.toPlainText()
+        excel_sheet.cell(row=27, column=1).value = self.textEdit_source_19.toPlainText()
+        excel_sheet.cell(row=28, column=1).value = self.textEdit_source_20.toPlainText()
+        excel_sheet.cell(row=31, column=1).value = self.textEdit_source_21.toPlainText()
+        excel_sheet.cell(row=32, column=1).value = self.textEdit_source_22.toPlainText()
+        excel_sheet.cell(row=33, column=1).value = self.textEdit_source_23.toPlainText()
+        excel_sheet.cell(row=34, column=1).value = self.textEdit_source_24.toPlainText()
+        excel_sheet.cell(row=35, column=1).value = self.textEdit_source_25.toPlainText()
 
-class Table3Window(QtWidgets.QMainWindow, Ui_QueryTable3):
-    def __init__(self):
-        super().__init__()
-        self.setupUi(self)
-        self.pushButton.clicked.connect(self.click_close)
+        excel_sheet.cell(row=3, column=2).value = self.spinBox1.value()
+        excel_sheet.cell(row=4, column=2).value = self.spinBox_2.value()
+        excel_sheet.cell(row=5, column=2).value = self.spinBox_3.value()
+        excel_sheet.cell(row=6, column=2).value = self.spinBox_4.value()
+        excel_sheet.cell(row=7, column=2).value = self.spinBox_5.value()
+        excel_sheet.cell(row=10, column=2).value = self.spinBox_6.value()
+        excel_sheet.cell(row=11, column=2).value = self.spinBox_7.value()
+        excel_sheet.cell(row=12, column=2).value = self.spinBox_8.value()
+        excel_sheet.cell(row=13, column=2).value = self.spinBox_9.value()
+        excel_sheet.cell(row=14, column=2).value = self.spinBox_10.value()
+        excel_sheet.cell(row=17, column=2).value = self.spinBox_11.value()
+        excel_sheet.cell(row=18, column=2).value = self.spinBox_12.value()
+        excel_sheet.cell(row=19, column=2).value = self.spinBox_13.value()
+        excel_sheet.cell(row=20, column=2).value = self.spinBox_14.value()
+        excel_sheet.cell(row=21, column=2).value = self.spinBox_15.value()
+        excel_sheet.cell(row=24, column=2).value = self.spinBox_16.value()
+        excel_sheet.cell(row=25, column=2).value = self.spinBox_17.value()
+        excel_sheet.cell(row=26, column=2).value = self.spinBox_18.value()
+        excel_sheet.cell(row=27, column=2).value = self.spinBox_19.value()
+        excel_sheet.cell(row=28, column=2).value = self.spinBox_20.value()
+        excel_sheet.cell(row=31, column=2).value = self.spinBox_21.value()
+        excel_sheet.cell(row=32, column=2).value = self.spinBox_22.value()
+        excel_sheet.cell(row=33, column=2).value = self.spinBox_23.value()
+        excel_sheet.cell(row=34, column=2).value = self.spinBox_24.value()
+        excel_sheet.cell(row=35, column=2).value = self.spinBox_25.value()
 
-    def click_close(self):
-        self.close()
+        excel_sheet.cell(row=3, column=3).value = self.textEdit_type_1.toPlainText()
+        excel_sheet.cell(row=4, column=3).value = self.textEdit_type_2.toPlainText()
+        excel_sheet.cell(row=5, column=3).value = self.textEdit_type_3.toPlainText()
+        excel_sheet.cell(row=6, column=3).value = self.textEdit_type_4.toPlainText()
+        excel_sheet.cell(row=7, column=3).value = self.textEdit_type_5.toPlainText()
+        excel_sheet.cell(row=10, column=3).value = self.textEdit_type_6.toPlainText()
+        excel_sheet.cell(row=11, column=3).value = self.textEdit_type_7.toPlainText()
+        excel_sheet.cell(row=12, column=3).value = self.textEdit_type_8.toPlainText()
+        excel_sheet.cell(row=13, column=3).value = self.textEdit_type_9.toPlainText()
+        excel_sheet.cell(row=14, column=3).value = self.textEdit_type_10.toPlainText()
+        excel_sheet.cell(row=17, column=3).value = self.textEdit_type_11.toPlainText()
+        excel_sheet.cell(row=18, column=3).value = self.textEdit_type_12.toPlainText()
+        excel_sheet.cell(row=19, column=3).value = self.textEdit_type_13.toPlainText()
+        excel_sheet.cell(row=20, column=3).value = self.textEdit_type_14.toPlainText()
+        excel_sheet.cell(row=21, column=3).value = self.textEdit_type_15.toPlainText()
+        excel_sheet.cell(row=24, column=3).value = self.textEdit_type_16.toPlainText()
+        excel_sheet.cell(row=25, column=3).value = self.textEdit_type_17.toPlainText()
+        excel_sheet.cell(row=26, column=3).value = self.textEdit_type_18.toPlainText()
+        excel_sheet.cell(row=27, column=3).value = self.textEdit_type_19.toPlainText()
+        excel_sheet.cell(row=28, column=3).value = self.textEdit_type_20.toPlainText()
+        excel_sheet.cell(row=31, column=3).value = self.textEdit_type_21.toPlainText()
+        excel_sheet.cell(row=32, column=3).value = self.textEdit_type_22.toPlainText()
+        excel_sheet.cell(row=33, column=3).value = self.textEdit_type_23.toPlainText()
+        excel_sheet.cell(row=34, column=3).value = self.textEdit_type_24.toPlainText()
+        excel_sheet.cell(row=35, column=3).value = self.textEdit_type_25.toPlainText()
 
-class Table4Window(QtWidgets.QMainWindow, Ui_QueryTable4):
-    def __init__(self):
-        super().__init__()
-        self.setupUi(self)
-        self.pushButton.clicked.connect(self.click_close)
-
-    def click_close(self):
-        self.close()
+        excel_file.save(filename=self.tableName + ".xlsx")
+        msg = QMessageBox()
+        msg.setText("Данные сохранены в файле " + self.tableName + ".xlsx")
+        msg.setWindowTitle("Information")
+        msg.exec_()
 
 class MainWindow(QtWidgets.QMainWindow):
     def __init__(self):
@@ -123,26 +186,24 @@ class MainWindow(QtWidgets.QMainWindow):
         self.onto.show()
 
     def click_table1(self):
-        self.table1 = Table1Window()
+        self.table1 = TableWindow('Table1')
         self.table1.show()
 
     def click_table2(self):
-        self.table2 = Table2Window()
+        self.table2 = TableWindow('Table2')
         self.table2.show()
 
     def click_table3(self):
-        self.table3 = Table3Window()
+        self.table3 = TableWindow('Table3')
         self.table3.show()
 
     def click_table4(self):
-        self.table4 = Table4Window()
+        self.table4 = TableWindow('Table4')
         self.table4.show()
 
     def click_gisto(self):
         self.gisto = GistoWindow()
         self.gisto.show()
-
-
 
 
 def main():
